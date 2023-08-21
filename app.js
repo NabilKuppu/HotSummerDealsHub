e5365ea44e568f069954a7df15f10679a5f03cde
NabilKuppu <mohammadnabil1510597088@gmail.com>
2023-08-21 15:59:18 +0600

    Completed the Project

---
+++ app.js
@@ -0,0 +1,48 @@
+let element = "";
+let totalPrice = 0;
+
+const sell = (sellData) => {
+  return sellData.innerText;
+};
+
+function total(data) {
+  const mainContainer = document.getElementById("cart-items");
+  element = data.childNodes[3].childNodes[3].innerText;
+  const createElement = document.createElement("li");
+  createElement.innerHTML = element;
+  mainContainer.appendChild(createElement);
+  const price =
+    data.childNodes[3].childNodes[4].nextSibling.childNodes[0].innerText;
+  totalPrice += parseInt(price);
+  document.getElementById("totalPrice").innerText = totalPrice;
+
+  if (totalPrice >= 200) {
+    couponBtn.removeAttribute("disabled");
+  }
+  document.getElementById("btn-purchase").removeAttribute("disabled");
+  document.getElementById("total").innerText = totalPrice;
+}
+function apply() {
+  const coupounValue = document.getElementById("couponValue").value;
+  if (coupounValue === "SELL200") {
+    const totalPrice = parseInt(
+      document.getElementById("totalPrice").innerText
+    );
+    const discountPrice = (totalPrice * 0.2).toFixed(2);
+    document.getElementById("discountPrice").innerText = discountPrice;
+    document.getElementById("total").innerText = totalPrice - discountPrice;
+  } else {
+    alert(
+      `Sorry!😪😪 This is not a valid coupon.You Can Use "SELL200" to get 20% Discount🥰🥰😘`
+    );
+  }
+}
+function modalBtn() {
+  document.getElementById("discountPrice").innerText = "00";
+  document.getElementById("total").innerText = "00";
+  document.getElementById("totalPrice").innerText = "00";
+  document.getElementById("cart-items").innerText = "";
+  document.getElementById("couponValue").value = "";
+  document.getElementById("btn-purchase").disabled = true;
+  document.getElementById("couponBtn").disabled = true;
+}
